cd9777baa23a5f2ade2a60079da57f745a2218e6
Andy VanWagoner <thetalecrafter@gmail.com>
2012-10-08 11:27:25 -0400

    Fix a couple small issues. Added CSV & DSV parse/stringify.

--- src/workbook.js
+++ src/workbook.js
@@ -9,7 +9,7 @@ function WorkBook() {
 
 WorkBook.prototype = {
 	window: { x:0, y:0, width:19200, height:10800 },
-	activeTab: null,
+	activeTab: 1, // 1-indexed
 	creator: null,
 	lastModifiedBy: null,
 	created: new Date(),
--- src/worksheet.js
+++ src/worksheet.js
@@ -11,6 +11,7 @@ WorkSheet.prototype = {
 	name: null,
 	table: null,
 	selection: null,
+	index: 1, // 1-indexed
 	margin: { top:0.75, right:0.7, bottom:0.75, left:0.7, header:0.3, footer:0.3 }
 };
 
