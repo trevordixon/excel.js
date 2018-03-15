deeb1a598fb84a9313d6de2f038f1db4623a0bba
Nikolay <catamphetamine@users.noreply.github.com>
2018-03-16 00:16:10 +0300

    Rewrote in ES6
    Your code seems to be very old.
    We rewrote it in modern ES6.
    Also your nested `require()`s were causing issues with Webpack bundling (it didn't bundle them) so now it also works.
    CC @oseibonsu

--- excelParser.js
+++ excelParser.js
@@ -1,165 +1,166 @@
-var Promise = require('node-promise'),
-	defer = Promise.defer,
-	when = Promise.when,
-	all = Promise.all,
-	_ = require('underscore');
+import fs from 'fs';
+import Stream from 'stream';
+import unzip from 'unzip2';
+import libxmljs from 'libxmljs';
 
 function extractFiles(path, sheet) {
-	var unzip = require('unzip2'),
-		deferred = defer();
+  const files = {
+    strings: {},
+    sheet: {},
+    'xl/sharedStrings.xml': 'strings',
+    ['xl/worksheets/sheet' + sheet + '.xml']: 'sheet'
+  };
 
-	var files = {
-		strings: {
-			deferred: defer()
-		},
-		sheet: {
-			deferred: defer()
-		},
-		'xl/sharedStrings.xml': 'strings'	
-	};
-	files['xl/worksheets/sheet' + sheet + '.xml'] = 'sheet';
-	var noop = function () {};
-	
-	var srcStream = path instanceof require('stream') ?
-		path :
-		require('fs').createReadStream(path);
+  const stream = path instanceof Stream ? path : fs.createReadStream(path);
 
-	srcStream
-		.pipe(unzip.Parse())
-		.on('error', function(err) {
-			deferred.reject(err);
-		})
-		.on('end', function(){
-			deferred.resolve();
-		})
-		.on('entry', function(entry) {
-			if (files[entry.path]) {
-				var contents = '';
-				entry.on('data', function(data) {
-					contents += data.toString();
-				}).on('end', function() {
-					files[files[entry.path]].contents = contents;
-					files[files[entry.path]].deferred.resolve();
-				});
-			} else {
-        entry.on('data', noop); // otherwise unzip.Parse() will hang forever on this entry on some xlsx files
-      }
-		});
+  return new Promise((resolve, reject) => {
+    const filePromises = [];
 
-	when(all(_.pluck(files, 'deferred')), function() {
-		deferred.resolve(files);
-	});
-
-	return deferred.promise;
+    stream
+      .pipe(unzip.Parse())
+      .on('error', reject)
+      .on('close', () => {
+        Promise.all(filePromises).then(() => resolve(files));
+      })
+      // For some reason `end` event is not emitted.
+      // .on('end', () => {
+      //   Promise.all(filePromises).then(() => resolve(files));
+      // })
+      .on('entry', (entry) => {
+        const file = files[entry.path];
+        if (file) {
+          let contents = '';
+          filePromises.push(new Promise((resolve) => {
+            entry
+              .on('data', data => contents += data.toString())
+              .on('end', () => {
+                files[file].contents = contents;
+                resolve();
+              });
+          }));
+        } else {
+          // // Consume data from stream.
+          // // otherwise unzip.Parse() will hang forever on this entry on some xlsx files.
+          // entry.on('data', () => {});
+          entry.autodrain();
+        }
+      });
+  });
 }
 
 function calculateDimensions (cells) {
-    var comparator = function (a, b) { return a-b; };
-    var allRows = _(cells).map(function (cell) { return cell.row; }).sort(comparator),
-        allCols = _(cells).map(function (cell) { return cell.column; }).sort(comparator),
-        minRow = allRows[0],
-        maxRow = _.last(allRows),
-        minCol = allCols[0],
-        maxCol = _.last(allCols);
+  const comparator = (a, b) => a - b;
+  const allRows = cells.map(cell => cell.row).sort(comparator);
+  const allCols = cells.map(cell => cell.column).sort(comparator);
+  const minRow = allRows[0];
+  const maxRow = allRows[allRows.length - 1];
+  const minCol = allCols[0];
+  const maxCol = allCols[allCols.length - 1];
 
-    return [
-        {row: minRow, column: minCol},
-        {row: maxRow, column: maxCol}
-    ];
+  return [
+    { row: minRow, column: minCol },
+    { row: maxRow, column: maxCol }
+  ];
 }
 
 function extractData(files) {
-	try {
-		var libxmljs = require('libxmljs'),
-			sheet = libxmljs.parseXml(files.sheet.contents),
-			strings = libxmljs.parseXml(files.strings.contents),
-			ns = {a: 'http://schemas.openxmlformats.org/spreadsheetml/2006/main'},
-			data = [];
-		var values = strings.find('//a:si', ns).map(string => string.find('.//a:t[not(ancestor::a:rPh)]', ns).map(t => t.text()).join(''));
-	} catch(parseError){
-	   return [];
-    	}
+  const ns = { a: 'http://schemas.openxmlformats.org/spreadsheetml/2006/main' };
+  let sheet;
+  let values;
+  const data = [];
 
-	var colToInt = function(col) {
-		var letters = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-		col = col.trim().split('');
-		
-		var n = 0;
+  try {
+    sheet = libxmljs.parseXml(files.sheet.contents);
+    values = libxmljs.parseXml(files.strings.contents)
+      .find('//a:si', ns)
+      .map(string => string.find('.//a:t[not(ancestor::a:rPh)]', ns)
+      .map(t => t.text()).join(''));
+  } catch(parseError){
+    return [];
+  }
 
-		for (var i = 0; i < col.length; i++) {
-			n *= 26;
-			n += letters.indexOf(col[i]);
-		}
+  function colToInt(col) {
+    const letters = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    col = col.trim().split('');
 
-		return n;
-	};
+    let n = 0;
 
-	var CellCoords = function(cell) {
-		cell = cell.split(/([0-9]+)/);
-		this.row = parseInt(cell[1]);
-		this.column = colToInt(cell[0]);
-	};
+    for (let i = 0; i < col.length; i++) {
+      n *= 26;
+      n += letters.indexOf(col[i]);
+    }
 
-	var na = { value: function() { return ''; },
-        text:  function() { return ''; } };
+    return n;
+  };
 
-	var Cell = function(cellNode) {
-		var r = cellNode.attr('r').value(),
-			type = (cellNode.attr('t') || na).value(),
-			value = (cellNode.get('a:v', ns) || na ).text(),
-			coords = new CellCoords(r);
+  const na = {
+    value: () => '',
+    text: () => ''
+  };
 
-		this.column = coords.column;
-		this.row = coords.row;
-		this.value = value;
-		this.type = type;
-	};
+  class CellCoords {
+    constructor(cell) {
+      cell = cell.split(/([0-9]+)/);
+      this.row = parseInt(cell[1]);
+      this.column = colToInt(cell[0]);
+    }
+  }
 
-	var cellNodes = sheet.find('/a:worksheet/a:sheetData/a:row/a:c', ns);
-	var cells = _(cellNodes).map(function (node) {
-		return new Cell(node);
-	});
+  class Cell {
+    constructor(cellNode) {
+      const r = cellNode.attr('r').value();
+      const type = (cellNode.attr('t') || na).value();
+      const value = (cellNode.get('a:v', ns) || na ).text();
+      const coords = new CellCoords(r)
 
-	var d = sheet.get('//a:dimension/@ref', ns);
-	if (d) {
-		d = _.map(d.value().split(':'), function(v) { return new CellCoords(v); });
-	} else {
-        d = calculateDimensions(cells)
-	}
+      this.column = coords.column;
+      this.row = coords.row;
+      this.value = value;
+      this.type = type;
+    }
+  }
 
-	var cols = d[1].column - d[0].column + 1,
-		rows = d[1].row - d[0].row + 1;
+  const cells = sheet.find('/a:worksheet/a:sheetData/a:row/a:c', ns).map(node => new Cell(node));
 
-	_(rows).times(function() {
-		var _row = [];
-		_(cols).times(function() { _row.push(''); });
-		data.push(_row);
-	});
+  let d = sheet.get('//a:dimension/@ref', ns);
+  if (d) {
+    d = d.value().split(':').map(_ => new CellCoords(_));
+  } else {
+    d = calculateDimensions(cells);
+  }
 
-	_.each(cells, function(cell) {
-		var value = cell.value;
+  const cols = d[1].column - d[0].column + 1;
+  const rows = d[1].row - d[0].row + 1;
 
-		if (cell.type == 's') {
-			value = values[parseInt(value)];
-		}
-		
-		if (data[cell.row - d[0].row]) {
-			data[cell.row - d[0].row][cell.column - d[0].column] = value;
-		}
-	});
+  times(rows, () => {
+    const row = [];
+    times(cols, () => row.push(''));
+    data.push(row);
+  });
 
-	return data;
+  for (const cell of cells) {
+    let value = cell.value;
+
+    if (cell.type == 's') {
+      value = values[parseInt(value)];
+    }
+
+    if (data[cell.row - d[0].row]) {
+      data[cell.row - d[0].row][cell.column - d[0].column] = value;
+    }
+  }
+
+  return data;
 }
 
-module.exports = function parseXlsx(path, sheet, cb) {
-	if (typeof cb === 'undefined') {
-		cb = sheet;
-		sheet = '1';
-	}
-	extractFiles(path, sheet).then(function(files) {
-		cb(null, extractData(files));
-	},
-	function(err) {
-		cb(err);
-	});
+export default function parseXlsx(path, sheet = '1') {
+  return extractFiles(path, sheet).then((files) => extractData(files));
 };
+
+function times(n, action) {
+  let i = 0;
+  while (i < n) {
+    action();
+    i++;
+  }
+}
